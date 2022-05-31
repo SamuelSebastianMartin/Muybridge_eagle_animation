84c60f08f5430d1ba94ec9172350458b7a9c87a6
Sam <sm2@soas.ac.uk>
2022-05-31 18:22:19 +0100

    added a takeoff sequence, to limited effect

--- README.md
+++ README.md
@@ -7,7 +7,16 @@ bringing to life some old creatures!
 
 Sprite dimensions.
 ------------------
-
+Now using the improved sprite sheet, where the eye remains
+in more or less the same place within the frame.
 ![](img/eagle_sprite_sheet_5frame_by2.png)
+The top row is flight, the second row is takeoff.
+
 W: 526 * 6
+
 H: 493 * 6
+
+As the original image series was not of a bird in full flight,
+but rather a bird taking off and beginning to fly, the flight
+is not smooth. Also, the bird was tethered, further lowering
+the authenticity of the flight sequence.
--- script.js
+++ script.js
@@ -4,28 +4,47 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const eagleImage = new Image();
-eagleImage.src = 'img/eagle_sprite_sheet.png';
-const spriteWidth = 461;
-const spriteHeight = 498;
+eagleImage.src = 'img/eagle_sprite_sheet_5frame_by2.png';
+const spriteWidth = 526;
+const spriteHeight = 493;
 let frameX = 0;
 let gameFrame = 0;
+let takeoffClimbRate = 9; // speed of assent to flying level
 const staggerFrame = 9;
 let speed = 6;
+let takeoff = true; // will takeoff sprite row be used.
 
 function animate(){
 
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
   // Animate eagle
-  ctx.drawImage(eagleImage, frameX * spriteWidth, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-  // Slow the animation
-  if (gameFrame % staggerFrame == 0){
-    if (frameX < 7){
-      frameX ++;
+  // Takeoff - just 6 frames
+  if (takeoff) {
+    ctx.drawImage(eagleImage, frameX * spriteWidth, spriteHeight, spriteWidth, spriteHeight, 0, 70 - (frameX * takeoffClimbRate), CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Slow the animation
+    if (gameFrame % (staggerFrame) == 0){
+      if (frameX < 4){
+        frameX ++;
+      }
+      else {
+        takeoff = false;
+        frameX = 0;
+      }
     }
-    else frameX = 3;
+
+  }else { // Flying loop, repeating for ever.
+    ctx.drawImage(eagleImage, frameX * spriteWidth, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Slow the animation
+    if (gameFrame % staggerFrame == 0){
+      if (frameX < 4){
+        frameX ++;
+      }
+      else frameX = 0;
+    }
+
   }
+
   gameFrame++;
   requestAnimationFrame(animate);
 };
